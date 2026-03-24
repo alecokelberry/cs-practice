e37b2a54b4fce82f3a7826d58889e17b9f3c8d8c
alecokelberry <y47k2zpw2r@privaterelay.appleid.com>
2026-03-23 18:08:56 -0600

    Fix indentation, add further comments

--- java/01_oop_fundamentals/project-1/Main.java
+++ java/01_oop_fundamentals/project-1/Main.java
@@ -1,15 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        Student student1 = new Student ("Alice", 1001, 3.5);
-        Student student2 = new Student ("Bob", 1002, 2.8);
-        Student student3 = new Student ("Carol", 1003, 3.9);
+        // Create 3 students
+        Student student1 = new Student("Alice", 1001, 3.5);
+        Student student2 = new Student("Bob", 1002, 2.8);
+        Student student3 = new Student("Carol", 1003, 3.9);
 
+        // Print each student
         System.out.println(student1);
         System.out.println(student2);
         System.out.println(student3);
 
+        // Print total student count
         System.out.println("Total students: " + Student.getStudentCount());
-
+        
+        // Try to create a student with a Bad GPA (5.0)
         try {
             Student student4 = new Student ("Dave", 1004, 5.0);
         } catch (IllegalArgumentException e) {
